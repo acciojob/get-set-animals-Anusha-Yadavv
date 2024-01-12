f6e28852ca664ae38f43ca673227d72dd56f795f
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2024-01-12 18:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 //complete this code
-class Animal {}
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
 
-class Dog extends Animal {}
+  get name() {
+    return this._name;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log(`${this.name} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+class Dog extends Animal {
+  bark() {
+    console.log(`${this.name} barks`);
+  }
+}
+
+class Cat extends Animal {
+  meow() {
+    console.log(`${this.name} meows`);
+  }
+}
+
+// Example usage:
+
+// Creating an instance of the Animal class
+const genericAnimal = new Animal("Generic Animal");
+console.log(`Animal: ${genericAnimal.name}`);
+genericAnimal.makeSound();
+
+// Creating an instance of the Dog class
+const dog = new Dog("Buddy");
+console.log(`Dog: ${dog.name}`);
+dog.makeSound();
+dog.bark();
+
+// Creating an instance of the Cat class
+const cat = new Cat("Whiskers");
+console.log(`Cat: ${cat.name}`);
+cat.makeSound();
+cat.meow();
